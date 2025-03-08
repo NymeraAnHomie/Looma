5b91296d238a65ac2edb7c00a4fa681aa506d4b7
NymeraAnHomie <frostiesnowman439@gmail.com>
2025-03-08 10:58:25 +0800

    Update ESP.lua

--- ESP.lua
+++ ESP.lua
@@ -1,7 +1,7 @@
 local Workspace, RunService, Players, CoreGui, Lighting = cloneref(game:GetService("Workspace")), cloneref(game:GetService("RunService")), cloneref(game:GetService("Players")), game:GetService("CoreGui"), cloneref(game:GetService("Lighting"))
 
 local ESP = {
-    Enabled = false,
+    Enabled = true,
     TeamCheck = false,
     MaxDistance = math.huge,
     FontSize = 11,
